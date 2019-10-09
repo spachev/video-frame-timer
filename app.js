d8681efe5a6cb2487ec2151b83494ad7993e0f8a
Sasha Pachev <sasha@asksasha.com>
2019-10-08 22:10:39 -0600

    Format time in hh:mm:ss.cc format, basic time recording.

--- app.js
+++ app.js
@@ -2,9 +2,38 @@
 var FRAME_T = 1.0/29.97;
 var time_offset = 0.0;
 
+function fmt_time(t)
+{
+	var ss, mm, hh;
+	var sign = "";
+
+	if (t < 0)
+	{
+		sign = "-";
+		t = -t;
+	}
+
+	ss = t % 60;
+	t -= ss;
+	ss = ss.toFixed(2);
+	t = Math.round(t);
+	t /= 60;
+	mm = t % 60;
+	t -= mm;
+	t /= 60;
+	hh = Math.round(t);
+	var res_arr = [];
+	if (hh)
+		res_arr.push(hh);
+	res_arr.push(mm);
+	res_arr.push(ss);
+	return sign + res_arr.map(function (t) {
+		if (t < 10) t = "0" + t; return t;}).join(":");
+}
+
 function update_time()
 {
-	$('#time-overlay').text((get_time() - time_offset).toFixed(2));
+	$('#time-overlay').text(fmt_time(get_time() - time_offset));
 }
 
 function handle_file_select(e)
@@ -13,16 +42,6 @@ function handle_file_select(e)
 	var f = e.target.files[0];
 	$('#video').attr("src", URL.createObjectURL(f));
 	$('#video').bind("timeupdate", update_time);
-}
-
-function next_frame()
-{
-	inc_time_pos(FRAME_T);
-}
-
-function prev_frame()
-{
-	inc_time_pos(-FRAME_T);
 }
 
 function get_time()
@@ -45,24 +64,44 @@ function show_status(msg)
 	$('#status').text(msg);
 }
 
-function start_time()
-{
-	time_offset = get_time();
-	update_time();
-}
+window.onload = function () {
+	var container = new Vue({
+		el: '#container',
+		data: {
+			overlay_buttons: [
+				{id: "prev-frame", text: "<<" },
+				{id: "next-frame", text: ">>"},
+				{id: "start-time", text: "Start"},
+				{id: "record-time", text: "Time"},
+			],
+			times: []
+		},
+		methods: {
+			handle_click: function (b) {
+				var cb_name = b.id.replace("-", "_");
+				this[cb_name]();
+			},
+			next_frame: function ()
+			{
+				inc_time_pos(FRAME_T);
+			},
+			prev_frame:function ()
+			{
+				inc_time_pos(-FRAME_T);
+			},
+			start_time: function ()
+			{
+				time_offset = get_time();
+				update_time();
+			},
+			record_time: function()
+			{
+				var t = get_time() - time_offset;
+				var t_o = {t:t, fmt_t: fmt_time(t)};
+				var vm = this;
+				vm.times.push(t_o);
+			}
 
-var video_container = new Vue({
-	el: '#video-container',
-	data: {
-		overlay_buttons: [
-			{id: "prev-frame", cb: prev_frame, text: "<<" },
-			{id: "next-frame", cb: next_frame, text: ">>"},
-			{id: "start-time", cb: start_time,  text: "Start"}
-		]
-	},
-	methods: {
-		handle_click: function (b) {
-			b.cb();
 		}
-	}
-});
+	});
+}
